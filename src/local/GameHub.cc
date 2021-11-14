08d8a05bc3dbb3d55f11b425fe0fefbdd08b0a7d
ahugeat <hugeat.arthur@gmail.com>
2021-11-14 20:29:30 +0100

    [game] Change build generator for windows

--- src/local/GameHub.cc
+++ src/local/GameHub.cc
@@ -4,7 +4,7 @@
 
 namespace flo {
   GameHub::GameHub()
-  : gf::GameManager("Game Flo", { GAME_DATA_DIR, GAME_SOURCE_DATA_DIR })
+  : gf::GameManager("Game Flo", { GAME_DATA_DIR })
   , menu(*this)
   , level(*this) {
     pushScene(level);
